9d28ae768faddded86c2eaf5c2ffc28159b55569
Tom Dane <tdane@tesla.com>
2019-09-12 16:56:27 -0700

    Init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+
+namespace contactFormTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetToken();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void GetToken()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless");
+
+            var directory = Environment.CurrentDirectory;
+            var driver = new ChromeDriver(directory, chromeOptions);
+
+            driver.Navigate().GoToUrl("https://ocius.com.au");
+            driver.Quit();
+        }
+    }
+}
