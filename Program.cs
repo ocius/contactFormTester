b5a91b3927dfa6fb749acfcd8a2fb5be867c4d30
Tom Dane <tdane@tesla.com>
2019-09-12 18:11:07 -0700

    Can get success message

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
+using System.Threading;
 
 namespace contactFormTests
 {
@@ -20,11 +23,22 @@ namespace contactFormTests
             var driver = new ChromeDriver(directory, chromeOptions);
 
             driver.Navigate().GoToUrl("https://ocius.com.au/usv#technical");
+
             var textInput = driver.FindElementByName("fullName").GetAttribute("placeholder");
+            driver.FindElementByName("fullName").SendKeys("Test - Tom Dane");
+            driver.FindElementByName("email").SendKeys("ocius@tomdane.com");
+            driver.FindElementByName("interest").SendKeys("Testing");
+            driver.FindElementByXPath("//*[@id='___gatsby']/div/div/div/div[3]/form/button").Click();
 
+            var messageXPath = "//*[@id='___gatsby']/div/div/div/div[3]/form/span";
 
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            var element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(messageXPath)));
+
+            var message = element.Text;
             driver.Quit();
-            return textInput;
+
+            return message;
 
         }
     }
