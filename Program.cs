cb36dd652d6f4efa5fdf45ed3b825f9654e1d3f1
Tom Dane <tdane@tesla.com>
2019-09-12 18:56:51 -0700

    Add gmail API

--- Program.cs
+++ Program.cs
@@ -1,45 +1,69 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace contactFormTests
+namespace GmailQuickstart
 {
     class Program
     {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
         static void Main(string[] args)
         {
-            var foo = GetToken();
-            Console.WriteLine(foo);
-        }
+            UserCredential credential;
 
-        private static string GetToken()
-        {
-            var chromeOptions = new ChromeOptions();
-            //chromeOptions.AddArgument("headless");
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
 
-            var directory = Environment.CurrentDirectory;
-            var driver = new ChromeDriver(directory, chromeOptions);
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
 
-            driver.Navigate().GoToUrl("https://ocius.com.au/usv#technical");
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
 
-            var textInput = driver.FindElementByName("fullName").GetAttribute("placeholder");
-            driver.FindElementByName("fullName").SendKeys("Test - Tom Dane");
-            driver.FindElementByName("email").SendKeys("ocius@tomdane.com");
-            driver.FindElementByName("interest").SendKeys("Testing");
-            driver.FindElementByXPath("//*[@id='___gatsby']/div/div/div/div[3]/form/button").Click();
-
-            var messageXPath = "//*[@id='___gatsby']/div/div/div/div[3]/form/span";
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            var element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(messageXPath)));
-
-            var message = element.Text;
-            driver.Quit();
-
-            return message;
-
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
+            Console.Read();
         }
     }
 }
--- readme.md
+++
@@ -1,5 +0,0 @@
-# Installation
-
-- Clone the repo
-- Download chrome driver 76 https://chromedriver.chromium.org/downloads
-- dotnet run
