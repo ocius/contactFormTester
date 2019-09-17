16e432cd6528751cf798ac3734ef878b324e14f9
Tom Dane <tdane@tesla.com>
2019-09-17 08:14:24 -0700

    Add ability to send email

--- Program.cs
+++ Program.cs
@@ -4,12 +4,9 @@ using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Net.Mail;
 
 namespace GmailQuickstart
 {
@@ -17,10 +14,15 @@ namespace GmailQuickstart
     {
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/gmail-dotnet-quickstart.json
-        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string[] Scopes = { GmailService.Scope.GmailSend };
         static string ApplicationName = "Gmail API .NET Quickstart";
 
-        static void Main(string[] args)
+        static void Main()
+        {
+            SendFailureEmail();
+        }
+
+        private static void SendFailureEmail()
         {
             UserCredential credential;
 
@@ -46,24 +48,34 @@ namespace GmailQuickstart
                 ApplicationName = ApplicationName,
             });
 
-            // Define parameters of request.
-            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+            //Create Message
+            MailMessage mail = new MailMessage
+            {
+                Subject = "FAILURE: Could not submit contact form",
+                Body = "The attempt to submit the form for Ocuis tech specs failed",
+                From = new MailAddress("tjdane@gmail.com"),
+                IsBodyHtml = true
+            };
+            mail.To.Add(new MailAddress("tom@ocius.com.au"));
+            mail.To.Add(new MailAddress("tjdane@gmail.com"));
 
-            // List labels.
-            IList<Label> labels = request.Execute().Labels;
-            Console.WriteLine("Labels:");
-            if (labels != null && labels.Count > 0)
+            MimeKit.MimeMessage mimeMessage = MimeKit.MimeMessage.CreateFromMailMessage(mail);
+
+            Message message = new Message
             {
-                foreach (var labelItem in labels)
-                {
-                    Console.WriteLine("{0}", labelItem.Name);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No labels found.");
-            }
-            Console.Read();
+                Raw = Base64UrlEncode(mimeMessage.ToString())
+            };
+
+            service.Users.Messages.Send(message, "tjdane@gmail.com").Execute();
+        }
+
+        private static string Base64UrlEncode(string input)
+        {
+            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            return Convert.ToBase64String(inputBytes)
+              .Replace('+', '-')
+              .Replace('/', '_')
+              .Replace("=", "");
         }
     }
 }
